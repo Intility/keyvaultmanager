ba3516772fefa5313a54b993a1d91ce0713a82ec
Jonatan Vik <jonatan.vik@intility.no>
2022-09-01 10:46:51 +0200

    ci: 👷 added test and coverage

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const config = {
   collectCoverage: true,
   collectCoverageFrom: ['./**/*.js', '!./jest.config.js', '!**/coverage/**'],
   setupFiles: ['<rootDir>/__test__/setEnvVars.js'],
+  coverageReporters: ['clover', 'json', 'lcov', 'text', 'cobertura'],
 };
 
 module.exports = config;
